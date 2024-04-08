981034d1fe213308fdb28ff458c3364ea4333699
song hyun <kny448600@gmail.com>
2024-04-08 21:18:00 +0900

    4-8

---
+++ 4_8/2518.js
@@ -0,0 +1,53 @@
+const myName = "sang hyun"
+const email = "kny448600@gmail.com"
+const hello = 'Hello ${myName}??'
+
+console.log(hello)
+
+const number = 10;
+const number2 = 10.55;
+
+const checked = true;
+const isShow = False;
+
+let undef;
+const obj = {abc:123};
+console.log(obj.)
+console.log(obj.)
+console.log(obj.)
+
+
+let name = null;
+console,log(name);
+name = "ihihi";
+console.log(name);
+
+const user = {
+    userName : "jh",
+    age : 18,
+    isVAalid : true 
+};
+console.log(user.userName);
+console.log(user.age);
+console,log(user.isVAalid);
+
+const fruits = ['Apple', 'Banana', 'Cherry'];
+console.log(fruits[0]);
+
+
+var aaa = "A";
+aaa = "B"
+console.log(aaa);
+
+isShow = true;
+if (isShow) {
+
+
+
+function abc() {
+    var aaa = "A";
+    console.log(aaa);
+}
+abc();
+}
+console.log(aaa);
