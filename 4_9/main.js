ae1d184b6ad4e54053dec77cbc3d515f937d31dd
sang hyun <kny448600@gmail.com>
2024-04-09 21:18:51 +0900

    4_9

---
+++ 4_9/main.js
@@ -0,0 +1,30 @@
+function funName() {
+
+}
+funName();
+
+function returnFuce() {
+    return 123;
+    console.log("함수가 실행되었음")
+}
+
+function sum(a,b){
+    return a+b;
+}
+const number = sum(1,2);
+const number2 = sum(3,4);
+const number3 = sum(5,6);
+
+function hello() {
+    console.log("hello");
+}
+hello();
+
+for(const key obj) {
+    console.log(key)
+    console.log(obj.name, obj.age);
+    console.log('key값: ${key}');
+    console.log('value값: ${obj[key]}');
+// }
+
+
